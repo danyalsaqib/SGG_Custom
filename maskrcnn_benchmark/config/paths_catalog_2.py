45a7b3c35fdb7212f8bfec933fee607f76aa0f94
danyal-saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-07-05 22:01:52 +0500

    Lol

--- maskrcnn_benchmark/config/paths_catalog_2.py
+++ maskrcnn_benchmark/config/paths_catalog_2.py
@@ -110,16 +110,16 @@ class DatasetCatalog(object):
             "ann_file": "cityscapes/annotations/instancesonly_filtered_gtFine_test.json"
         },
         "VG_stanford_filtered": {
-            "img_dir": "vg/VG_100K",
-            "roidb_file": "vg/VG-SGG.h5",
-            "dict_file": "vg/VG-SGG-dicts.json",
-            "image_file": "vg/image_data.json",
+            "img_dir": "SGG_Custom/vg/VG_100K",
+            "roidb_file": "SGG_Custom/vg/VG-SGG.h5",
+            "dict_file": "SGG_Custom/vg/VG-SGG-dicts.json",
+            "image_file": "SGG_Custom/vg/image_data.json",
         },
         "VG_stanford_filtered_with_attribute": {
-            "img_dir": "vg/VG_100K",
-            "roidb_file": "vg/VG-SGG-with-attri.h5",
-            "dict_file": "vg/VG-SGG-dicts-with-attri.json",
-            "image_file": "vg/image_data.json",
+            "img_dir": "SGG_Custom/vg/VG_100K",
+            "roidb_file": "SGG_Custom/vg/VG-SGG-with-attri.h5",
+            "dict_file": "SGG_Custom/vg/VG-SGG-dicts-with-attri.json",
+            "image_file": "SGG_Custom/vg/image_data.json",
         },
     }
 
